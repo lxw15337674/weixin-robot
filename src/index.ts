e2a21a828c0a2f3336d2b0ec6c54e470d8212c3e
Bhwa233 <404174262@qq.com>
2024-07-10 15:09:37 +0800

    chore: 更新wechaty-puppet-wechat4u依赖版本

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,10 @@ const CHROME_BIN = process.env.CHROME_BIN ?? {}
 const runRobot = async () => {
   const bot = WechatyBuilder.build({
     name: 'wechat-bot',
-    // puppet: 'wechaty-puppet-wechat', 
+    puppet: 'wechaty-puppet-wechat4u', // 如果有token，记得更换对应的puppet
+    // puppet: 'wechaty-puppet-wechat', // 如果 wechaty-puppet-wechat 存在问题，也可以尝试使用上面的 wechaty-puppet-wechat4u ，记得安装 wechaty-puppet-wechat4u
     puppetOptions: {
-      // uos: true,
+      uos: true,
       ...CHROME_BIN,
     },
   })
